9795e77881d1fe268145b6a077bbcfaef2bd3efb
Viktor Abudikhin <abudihin@gmail.com>
2022-11-06 22:04:40 +0300

    add id's

--- variables.tf
+++ variables.tf
@@ -5,10 +5,12 @@ variable "zone" {
 
 variable "cloud_id" {
   type = string
+  default = "b1goe3dambm08tdcqvcb"
 }
 
 variable "folder_id" {
   type = string
+  default = "b1g8orq0tmhvaddbqf99"
 }
 
 variable "image_id" {
@@ -19,4 +21,5 @@ variable "image_id" {
 
 variable "yc_token" {
   type = string
+  default = "t1.9euelZrOiZucl5KLlZuYzYubk5KNie3rnpWans6UkJTKnpuOjYvGjI6NyZLl8_cMfF9k-e9df1dR_d3z90wqXWT5711_V1H9zef1656VmsvNjZ7Ll5jJmcmWk5XGnY6Y7_0.JuxejDs3--FJaELj7PmkGQ3ekQpx1eB8ftLiRSPBqIz2bGG7dSyrtu4Bnmx_EXJSdGHOQglmLlwWSbVCNSCIAA"
 }
